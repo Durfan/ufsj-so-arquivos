cce26b3b983a45545c39a92e3f93fa45a1152b65
Durfan <cecilio@aluno.ufsj.edu.br>
2019-12-01 21:19:59 -0300

    ls TOC

--- src/cmds.c
+++ src/cmds.c
@@ -59,40 +59,36 @@ int ls(uint16_t argc, char **argv) {
 	char **path = tkenizer(argv[1],delim);
 
 	DataCluster cluster = readCL(9);
-	bool list = true;
-
 	int i=0, exists, block = 9;
+
 	while (path[i] != NULL) {
 		cluster = readCL(block);
 		exists  = dirSET(cluster,path[i]);
 		if (exists < 0) {
 			fprintf(stderr,"%s\n",strerror(ENOENT));
-			list = false;
-			break;
+			return 1;
 		}
 		else
 			block = exists;
 		i++;
 	}
 
-	if (list) {
-		int space = 0;
-		cluster = readCL(block);
-		printf("\u250C 0x%04X "BOLD"%s\n"NORM, block, argv1);
-		for (int i=0; i < 32; i++) {
-			if (cluster.dir[i].filename[0] != 0x0000) {
-				space++;
-				printf("\u251C\u2574 0x%04X ", cluster.dir[i].firstblock);
-				printf("%dB ", cluster.dir[i].size);
-				if (cluster.dir[i].attributes == 1)
-					printf(BOLD"%s"NORM, cluster.dir[i].filename);
-				else 
-					printf("%s", cluster.dir[i].filename);
-				putchar(0x0A);
-			}
+	int space = 0;
+	cluster = readCL(block);
+	printf("\u250C 0x%04X "BOLD"%s\n"NORM, block, argv1);
+	for (int i=0; i < 32; i++) {
+		if (cluster.dir[i].filename[0] != 0x0000) {
+			space++;
+			printf("\u251C\u2574 0x%04X ", cluster.dir[i].firstblock);
+			printf("%dB ", cluster.dir[i].size);
+			if (cluster.dir[i].attributes == 1)
+				printf(BOLD"%s"NORM, cluster.dir[i].filename);
+			else 
+				printf("%s", cluster.dir[i].filename);
+			putchar(0x0A);
 		}
-		printf("\u2514 cap: %d/32\n", space);
 	}
+	printf("\u2514 cap: %d/32\n", space);
 
 	free(argv1);
 	free(path);
@@ -109,8 +105,8 @@ int mkdir(uint16_t argc, char **argv) {
 
 	DataCluster cluster;
 	DirEntry folder;
-
 	int i=0, exists, block = 9;
+
 	while (path[i] != NULL) {
 		cluster = readCL(block);
 		exists  = dirSET(cluster,path[i]);
